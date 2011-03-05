b78aa31be01cfb4042eb7ad7a010902027d293be
Chris Freeman <cwfreeman@gmail.com>
2011-03-05 14:01:11 -0600

    Changed basic story.

--- createDiv.js
+++ createDiv.js
@@ -4,26 +4,20 @@ function S4() {
    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
 }
 
-$(document).ready($('#theBody').mousedown(function(e) {
+$(document).ready($('#cardCreator').mousedown(function(e) {
 		id = S4();
-		var div = makeCardDiv(e.pageX, e.pageY, id);
-		$(div).appendTo('#theDiv');
+		var title = $('#templateTitle').val();
+		var desc = $('#templateDesc').val();
+		var div = makeCardDiv(e.pageX, e.pageY, id, title, desc);
+		$(div).appendTo('body');
 		// send the div delta back to base
 	}
 ));
 
-$(document).ready($('html').keypress(function(e) {
-		var idSelector = "#" + id;
-		var currText = $(idSelector).text();
-		$(idSelector).text(currText + String.fromCharCode(e.which));
-		// send the div delta back to base...
-	}
-));
-
-function makeCardDiv(x, y, id) {
+function makeCardDiv(x, y, id, title, desc) {
 	return "<div id='" + id + "' class='card' style='top:" + y + ";left:" + x + ";'>"
-		+ "<div class='title'></div>"
+		+ "<div class='title'>" + title + "</div>"
 		+ "<hr/>"
-		+ "<div class='desc'></div>"
+		+ "<div class='desc'>" + desc + "</div>"
 		+ "</div>";
 }
--- tests.js
+++ tests.js
@@ -1,32 +1,9 @@
-var john, beforeCallbackTest, afterCallbackTest;
-
-describe("SpecIt with a before callback", function() {
-  var jane = {name: "Jane"};
-
+describe("Adding Content", function() {
   before(function() {
-    beforeCallbackTest = true;
-    john = {name: "John Doe"};
+  });
+  after(function() {
   });
 
   it("should support before", function() {
-    ok(beforeCallbackTest);
-    equal(afterCallbackTest, undefined);
-  });
-
-  it("should run before every test", function() {
-    john.name = "Wrong name";
-    jane.age = 26;
-  });
-
-  it("should run before every test", function() {
-    equals(john.name, "John Doe");
-  });
-
-  it("should not know attributes from another before callback", function() {
-    equals(john.age, undefined);
-  });
-
-  it("should not modify attributes on a local object if untouched in before", function() {
-    equals(jane.age, 26);
   });
 });
