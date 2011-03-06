1954eefbe002cad978347f0db8f4fce2c2abe1cd
Chris Freeman <cwfreeman@gmail.com>
2011-03-06 00:56:01 -0600

    Fixed tests, now that we're using jquery-ui instead of homegrown div moving.

--- tests.js
+++ tests.js
@@ -1,15 +1,11 @@
-describe("Moving Cards", function() {
+describe("Empty Behavior", function() {
   before(function() {
-	$(makeCardDiv(15, 15, 'abcdefg', 'title', 'desc')).appendTo('body');
+	//
   });
   after(function() {
-	$('#abcdefg').remove();
+	//
   });
 
-  it("should change the position to match the inputs", function() {
-	var card = $('#abcdefg');
-	moveCard(19, 33, card);
-	assert(card.position().top).should(eql, 33);
-	assert(card.position().left).should(eql, 19);
+  it("should do something verifiable", function() {
   });
 });
