64ceb7ade1eb93b1281b27cf9b5a6eba6fcef99c
Chris Freeman <cwfreeman@gmail.com>
2011-03-06 00:37:21 -0600

    Cards are created by button, not ad-hoc clicking.

--- createDiv.js
+++ createDiv.js
@@ -4,18 +4,34 @@ function S4() {
    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
 }
 
-$(document).ready($('#cardCreator').mousedown(function(e) {
+$(document).ready($('#cardCreator').click(function(e) {
 		id = S4();
 		var title = $('#templateTitle').val();
 		var desc = $('#templateDesc').val();
-		var div = makeCardDiv(e.pageX, e.pageY, id, title, desc);
+		var div = makeCardDiv(0, 300, id, title, desc);
 		$(div).appendTo('body');
+		$('.card').draggable({
+			containment: 'parent',
+			create: function(event, ui) {
+				$('<div>Created by...</div>').appendTo(event.target)
+			},
+			drag:   function(event, ui) {
+			},
+			start:  function(event, ui) {
+				//alert("Starting to drag...");
+				$(event.target).addClass('movingCard');
+			},
+			stop:   function(event, ui) {
+				$(event.target).removeClass('movingCard');
+			}
+		});
 		// send the div delta back to base
 	}
 ));
 
+
 function makeCardDiv(x, y, id, title, desc) {
-	return "<div id='" + id + "' class='card' style='top:" + y + ";left:" + x + ";'>"
+	return "<div id='" + id + "' class='card ui-draggable' style='top:" + y + ";left:" + x + ";'>"
 		+ "<div class='title'>" + title + "</div>"
 		+ "<hr/>"
 		+ "<div class='desc'>" + desc + "</div>"
--- tests.js
+++ tests.js
@@ -1,9 +1,15 @@
-describe("Adding Content", function() {
+describe("Moving Cards", function() {
   before(function() {
+	$(makeCardDiv(15, 15, 'abcdefg', 'title', 'desc')).appendTo('body');
   });
   after(function() {
+	$('#abcdefg').remove();
   });
 
-  it("should support before", function() {
+  it("should change the position to match the inputs", function() {
+	var card = $('#abcdefg');
+	moveCard(19, 33, card);
+	assert(card.position().top).should(eql, 33);
+	assert(card.position().left).should(eql, 19);
   });
 });
